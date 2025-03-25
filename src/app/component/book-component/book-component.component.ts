a6fe03798cb34698d6b3f28cbe1491d0c5481979
LeeroyRivera <leeroya506@gmail>
2025-03-24 21:32:45 -0600

    componentes iniciales añadidos

---
+++ src/app/component/book-component/book-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-component',
+  imports: [],
+  templateUrl: './book-component.component.html',
+  styleUrl: './book-component.component.css'
+})
+export class BookComponentComponent {
+
+}
