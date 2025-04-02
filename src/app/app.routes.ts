b59939cf303dbcca66fec37d5648e6ebe882417a
LeeroyRivera <leeroya506@gmail>
2025-04-02 11:20:01 -0600

    rutas añadidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: '**', component: NotFoundComponent }
+];
