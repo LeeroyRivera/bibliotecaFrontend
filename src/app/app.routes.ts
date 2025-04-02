b53159d45f824edf09a23b73843394291518d547
LeeroyRivera <leeroya506@gmail>
2025-04-02 11:44:11 -0600

    cambio rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
@@ -8,3 +9,9 @@ export const routes: Routes = [
     { path: 'home', component: HomePageComponent },
     { path: '**', component: NotFoundComponent }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+export class AppRoutingModule { }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { BooksDataService } from '../../services/books-data.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
+  providers: [BooksDataService]
 })
 export class HomePageComponent {
 
