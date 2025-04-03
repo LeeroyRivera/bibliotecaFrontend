444d96df771a61470bc2983ca7d993260ce1b8c9
LeeroyRivera <leeroya506@gmail>
2025-04-03 12:27:49 -0600

    cambios en login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./pages/login/login.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
