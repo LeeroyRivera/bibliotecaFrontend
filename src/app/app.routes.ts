79f59500520e3e35b22174ff3d009df7300aeb18
LeeroyRivera <leeroya506@gmail>
2025-04-07 22:16:09 -0600

    Actualizacion en el componente de routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
-    { path: 'home', component: HomePageComponent },
-    { path: '**', component: NotFoundComponent }
-];
 
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-export class AppRoutingModule { }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: '**', component: NotFoundComponent }
+
+];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
