c924e4c870dbdbd9e54618ac8cff4c0dfb7bf14c
LeeroyRivera <leeroya506@gmail>
2025-04-08 15:38:20 -0600

    Endpoints agregados

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private apiUrl = "http//192.168.1.96:3000"; 
+  constructor( private http: HttpClient ) { }
 
-  constructor() { }
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/usuarios`);
+  }
+
+  getUsersLogin(email: any, password: any): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/usuarios/${email}/${password}`);
+  }
+
+  getUser(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/usuarios/${id}`);
+  }
+
+  addUser(user: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/usuarios`, user);
+  }
+
+  updateUser(id: number, user: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/usuarios/${id}`, user);
+  }
+
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/usuarios/${id}`);
+  }
+
 }
