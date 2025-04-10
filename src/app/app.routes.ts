b260c9dd207c356b4b8191554556f8f5bee9c4df
LeeroyRivera <leeroya506@gmail>
2025-04-09 21:34:30 -0600

    ruta a libros añadida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,15 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { BooksPageComponent } from './pages/books-page/books-page.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomePageComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'registro', component: RegisterPageComponent }, 
+  { path: 'libros', component: BooksPageComponent },
+  { path: 'registro', component: RegisterPageComponent }, 
   { path: '**', component: NotFoundComponent },
-
 
 ];
