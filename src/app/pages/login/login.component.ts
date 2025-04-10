0f28cec611d181c6ad6dbf3db5697b72b825a85d
LeeroyRivera <leeroya506@gmail>
2025-04-10 03:16:00 -0600

    comentarios en login y registro añadidos

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -15,15 +15,21 @@ import { tap } from 'rxjs';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // Variables para el inicio de sesión con los nombres exactos de las tablas de la base de datos.
   email: string = '';
   password: string = '';
 
   constructor(private router: Router, private userService: UserService) {} 
 
   onSubmit() {
+    // Llamado al servicio user.services y enviado el objeto user para iniciar sesión en la base de datos.
+    // Se utiliza el método getUsersLogin para obtener los datos del usuario.
     this.userService.getUsersLogin({ correo: this.email, contra: this.password }).pipe(
       tap((response) => {
         console.log('Login response:', response);
+        // Manejo de la respuesta del servicio en consola con el proposito de debugging
+        // Si la respuesta es 'ok', se redirige a la página de inicio de sesión
+        // Si la respuesta no es 'ok', se muestra un mensaje de error
         if (response.usuariosEstado === true) {
           localStorage.setItem('user', JSON.stringify(response.usuariosNombre));
           this.router.navigate(['home']);
--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -16,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+  //variables para el registro de usuario con los nombres exactos de las tablas de la base de datos.
   usuariosNombre: string = '';
   usuariosCorreo: string = '';
   usuariosContra: string = '';
@@ -25,14 +26,20 @@ export class RegisterPageComponent {
   }
 
   register() {
+    
     const user = {
+      // Asignar los valores de los campos a las propiedades del objeto user
       usuariosNombre: this.usuariosNombre,
       usuariosCorreo: this.usuariosCorreo,
       usuariosContra: this.usuariosContra
     };
+    //llamado al servicio user.services y enviado el objeto user para registrar el usuario en la base de datos.
     this.userService.addUser(user).pipe(
       tap((response) => {
+        // Manejo de la respuesta del servicio en consola con el proposito de debugging
         console.log('Register response:', response);
+        // Si la respuesta es 'ok', se redirige a la página de inicio de sesión
+        // Si la respuesta no es 'ok', se muestra un mensaje de error
         if (response.msg === 'ok') {
           alert('Usuario registrado correctamente');
           this.router.navigate(['login']);
@@ -40,6 +47,6 @@ export class RegisterPageComponent {
           alert('Error al registrar el usuario');
         }
       })
-    ).subscribe();
+    ).subscribe();// Se suscribe al observable para que se ejecute la petición
   }
 }
